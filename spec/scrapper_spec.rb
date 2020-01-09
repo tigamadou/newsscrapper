e6ac36d50ae327a98e63f7255dc6755e11f0d11c
Amadou Ibrahim <amadou140589@gmail.com>
2020-01-10 00:26:34 +0100

    Rspec Test add_page  scrapper

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -7,7 +7,19 @@ RSpec.describe Scrapper do
     it "Initialize the object" do
       expect(scrapper.url).to eql("https://www.buzzfeed.com")
     end
+	end
+	
+	describe "#add_page" do
+    let(:scrapper) { Scrapper.new("https://www.buzzfeed.com") }
+    it "return true if the page is not in the list yed and add it to the list" do
+      expect(scrapper.add_page("https://www.buzzfeed.com/buzz")).to be_truthy
+    end
+    it "return false if the page is already in the list yed and add it to the list" do
+      expect(scrapper.add_page("https://www.buzzfeed.com/buzz")).to be_falsey
+    end
   end
+
+  
 
   
 	
