9168d5be1093b68d71983e2a7385dd05b539be4b
Amadou Ibrahim <amadou140589@gmail.com>
2020-01-09 02:13:26 +0100

    Scrapped class added

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -2,37 +2,83 @@ require "nokogiri"
 require 'httparty'
 require 'byebug'
 require 'uri'
+require 'json'
+require 'date'
 
 class Scrapper
-  attr_accessor :request
-	@auth = {:username => "genzaraky@gmail.com",:password=> "5uDADmMcFnY6UL7"}
-  def initialize(search_query)
-    @search_query = search_query.to_s
-    @request = {}
-    @request[:base_url] = "https://www.aliexpress.com/wholesale?"
-    @request[:pagination] = {
-      current: 1,
-      first: 1,
-      last: nil,
-      total: nil
-    }
-    @params = {}
-    search
+  attr_accessor :request,:yahoo,:url
+  def initialize(search_query=nil)
+    @search_query = search_query.to_s if !search_query.nil?
+    @filename = "default.json"
+    @selector=""
+    @ressource = {}    
+    @params = {}    
   end
 
   def search(search_query = nil)
     @search_query = search_query if !search_query.nil?
     @search_query = @search_query.gsub(" ", "+")
     @search_query = URI.encode(@search_query)
-    @request[:url] = @request[:base_url] + @search_query
-    @params.each do |k,v|
-      @request[:url] += "&#{k}=#{v}"
-    end
-    get_info
+    @url = "#{@base_url}search/site/#{@search_query}"
+    #@params.each do |k,v|
+    #  @url += "&#{k}=#{v}"
+    #end    
+  end
+  def get_url
+    @url
+  end
+  def get_count
+    @count
+  end
+  def get_filename
+    @filename
+  end
+  def scrap_all(url)    
+    @data = Nokogiri::HTML(HTTParty.get(url))   
   end
 
-  def get_info
-    @request[:data] = Nokogiri::HTML(HTTParty.get(@request[:url]))
-    byebug
+  def build
+    @elements = Array.new
+    list =@data.css(@selector)
+    @count = list.count
+    list.each do |article|
+      element = build_element(article)      
+      @elements << element      
+    end
+    
+    save_to_file
+    #byebug
+  end
+
+  def build_element(element)
+    {
+        
+      }
+  end
+
+  def save_to_file
+    File.open("data/#{@filename}","w") do |f|
+      f.write(JSON.pretty_generate(@elements))
+    end
+  end
+
+end
+
+class Buzzfeed < Scrapper
+  def initialize(search_query=nil)
+    @url = "https://www.buzzfeed.com/"
+    @filename = "buzzfeed.json"
+    @selector = "article.story-card"
+  end
+
+  def build_element(element)
+    {
+        title: element.css('a.js-card__link').text,
+        link: element.css('a')[0].attributes['href'].value,
+        excerpt: element.css('p.js-card__description').text,
+        cover: element.css('.card__image img').attr("src"),
+        source: "https://www.buzzfeed.com",
+        scraped_at: DateTime.now() 
+      }
   end
 end
