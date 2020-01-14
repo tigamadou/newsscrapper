07be06593e5a8116249d446ee768fb1b37f25669
Amadou Ibrahim <amadou140589@gmail.com>
2020-01-14 16:54:48 +0100

    Rspec tests naming edited

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -4,55 +4,41 @@ require_relative './../lib/scrapper.rb'
 RSpec.describe Scrapper do
   describe '#initialize' do
     let(:scrapper) { Scrapper.new }
-    it 'Initialize the object' do
+    it 'Initializes  the object' do
       expect(scrapper.url).to be_nil
     end
   end
 
   describe '#setup' do
     let(:scrapper) { Scrapper.new }
-    it 'Set up the scrapper with the given url' do
+    it 'Sets up the scrapper with the given url' do
       expect(scrapper.setup('https://www.buzzfeed.com')).to be_truthy
     end
 
-    it 'Should raise error  if the url is not provide' do
+    it 'raises an error if the URL is not provided' do
       expect { scrapper.setup }.to raise_error(ArgumentError)
     end
   end
 
   describe '#add_page' do
     let(:scrapper) { Scrapper.new }
-    it 'return true if the page is not in the list and add it to the list' do
+    it 'returns true if the page is not in the pages list' do
       scrapper.setup('https://www.buzzfeed.com')
       expect(scrapper.add_page('https://www.buzzfeed.com/buzz')).to be_truthy
     end
-    it 'return false if the page is already in the list ' do
+    it 'returns false if the page is already in the pages list ' do
       scrapper.setup('https://www.buzzfeed.com/hello')
       expect(scrapper.add_page('https://www.buzzfeed.com/hello')).to be_falsey
     end
   end
 
-  describe '#filename?' do
-    let(:scrapper) { Scrapper.new }
-    it 'return the file name to save the scraped data' do
-      scrapper.setup('https://www.buzzfeed.com')
-      expect(scrapper.filename?).to eql('default.json')
-    end
-
-    it 'return false if filename is nil' do
-      scrapper.setup('https://www.buzzfeed.com')
-      scrapper.filename = nil
-      expect(scrapper.filename?).to be_falsey
-    end
-  end
-
   describe '#scrap_page' do
     let(:scrapper) { Scrapper.new }
-    it 'return true if the page content has been scrapped' do
+    it 'returns true if the page content has been scrapped' do
       scrapper.setup('https://www.buzzfeed.com')
       expect(scrapper.scrap_page('https://www.buzzfeed.com')).to be_truthy
     end
-    it 'return false if the page content can not be scrapped' do
+    it 'returns false if the page content can not be scrapped' do
       scrapper.setup('https://www.newsweek.com')
       expect(scrapper.scrap_page('https://www.newsweek.com/')).to be_falsey
     end
@@ -60,11 +46,11 @@ RSpec.describe Scrapper do
 
   describe '#next_page' do
     let(:scrapper) { Scrapper.new }
-    it 'return false if no next page' do
+    it 'returns false if no next page' do
       scrapper.setup('https://www.buzzfeed.com')
       expect(scrapper.next_page).to be_falsey
     end
-    it 'return true if there is a next page' do
+    it 'returns true if there is a next page' do
       scrapper.setup('https://www.buzzfeed.com')
       scrapper.add_page('https://www.buzzfeed.com/buzz')
       expect(scrapper.next_page).to be_truthy
@@ -73,14 +59,14 @@ RSpec.describe Scrapper do
 
   describe '#build' do
     let(:scrapper) { Scrapper.new }
-    it 'return numéric if the page has been build' do
+    it 'returns a numeric value if the page has been built' do
       scrapper.setup('https://www.buzzfeed.com')
       scrapper.scrap_page('https://www.buzzfeed.com')
       scrapper.selector = 'article.story-card'
       expect(scrapper.build).to be_an(Numeric)
     end
 
-    it 'return 0 if the page has can\'t be build' do
+    it 'returns 0 if the page  can\'t be build' do
       scrapper.setup('https://www.buzzfeed.com')
       scrapper.scrap_page('https://www.buzzfeed.com')
       expect(scrapper.build).to eql(0)
@@ -89,7 +75,7 @@ RSpec.describe Scrapper do
 
   describe '#save_to_file' do
     let(:scrapper) { Scrapper.new }
-    it 'return true if the file name is saved ' do
+    it 'returns true if the file name is saved' do
       scrapper.setup('https://www.buzzfeed.com')
       scrapper.scrap_page('https://www.buzzfeed.com')
       scrapper.selector = 'article.story-card'
@@ -99,11 +85,11 @@ RSpec.describe Scrapper do
 
   describe '#parsing?' do
     let(:scrapper_x) { Scrapper.new }
-    it 'return false if there are no page to parse' do
+    it 'returns false if there are no page to parse' do
       expect(scrapper_x.parsing?).to be_falsey
     end
 
-    it 'return true if there are  page to parse' do
+    it 'returns true if there are  page to parse' do
       scrapper_x.add_page('https://www.buzzfeed.com/buzz')
       scrapper_x.add_page('https://www.buzzfeed.com/celebrity')
       expect(scrapper_x.parsing?).to be_truthy
